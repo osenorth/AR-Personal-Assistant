4c60bb301668bbb27e1b7f02e2d0364e45aee347
A-HK <kahani.0142@gmail.com>
2023-04-10 23:39:47 +0530

    testing webxr environment

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -6,7 +6,7 @@ export default function Test()
 {
     return (
         <>
-        <h1>Test</h1>
+        <h1>Testing Three.js WebXR Environment</h1>
         <XrHitModelContainer />
         </>
     )
