fb70d11d8109d0398cd6959d9708f854b2cdf9b8
A-HK <kahani.0142@gmail.com>
2023-06-28 21:39:39 +0530

    scaling

--- src/components/SpotifyXR/components/Right.js
+++ src/components/SpotifyXR/components/Right.js
@@ -196,7 +196,7 @@ function Right({ spotifyApi, chooseTrack }) {
               // />
               <>
               <Text color={'black'} font="/Inter_Medium_Regular.json" fontSize={0.2} position={[initialX+(2*index), initialY+1, initialZ]}>{track.title}</Text>
-              <Image imgSrc={'/Transparent_Panel.png'} scale={[2,2,1]} position={[initialX+(2*index), initialY, initialZ-1]} />
+              <Image imgSrc={'/Transparent_Panel.png'} scale={[1.5,2,1]} position={[initialX+(2*index)-(0.5), initialY, initialZ-1]} />
               <Image imgSrc={track.albumUrl} scale={[1,1,1]} position={[initialX+(2*index), initialY, initialZ]} />
               <Text color={'black'} font="/Inter_Medium_Regular.json" fontSize={0.2} position={[initialX+(2*index), initialY-1, initialZ]}>{track.artist}</Text>
               </>
