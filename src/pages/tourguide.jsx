2683f1c416f556b6fece977b15f08042c5cd861d
Harshil Jani <harshiljani2002@gmail.com>
2023-07-23 17:28:41 +0530

    Adding User Journey
    Signed-off-by: Harshil Jani <harshiljani2002@gmail.com>

--- src/pages/tourguide.jsx
+++ src/pages/tourguide.jsx
@@ -2,10 +2,14 @@ import Layout from "../containers/Layout";
 import TourGuideFeatures from "../components/Features/TourGuideFeatures";
 import tourGuideFeatures from "../data/TourGuideFeatureList";
 import Services from "../components/Services/Services";
+import UserJourney from "../components/UserJourney/UserJourney";
+import tourGuideJourneyData from "../data/TourGuideJourneyList";
+
 const TourGuide = () => {
   return (
     <Layout>
       <TourGuideFeatures />
+      <UserJourney journeyData={tourGuideJourneyData} />
       <Services servicesData={tourGuideFeatures} />
     </Layout>
   );
